2c9638bbc23c3bac6c4d9a1059883b01889f1eae
Almer Bolatov <almer.bolatov@gmail.com>
2018-01-16 15:38:22 +0600

    Change to vertical representation

---
+++ data.js
@@ -0,0 +1,137 @@
+const treeData = {
+    "name": "",
+    "children": [{
+        "name": "Бұжбан",
+        "description": "",
+        "children": [{
+            "name": "Мекет",
+            "description": "",
+            "children": [{
+                "name": "Орынбай",
+                "children": [{
+                    "name": "Нығмет",
+                    "children": [{
+                            "name": "Болат",
+                            "description": "",
+                            "children": [{
+                                    "name": "Қожабек",
+                                    "description": "Болатов Қожабек [Атыбай]",
+                                    "children": [{
+                                            "name": "Бейбіт",
+                                            "description": "Болатов Бейбіт",
+                                            "children": [{
+                                                    "name": "Жанас",
+                                                    "description": "Болатов Жанас",
+                                                    "children": []
+                                                },
+                                                {
+                                                    "name": "Әнуар",
+                                                    "description": "Қожабек Әнуар",
+                                                    "children": [{
+                                                        "name": "Нәби",
+                                                        "description": "Бейбіт Нәби",
+                                                        "children": []
+                                                    }]
+                                                },
+                                                {
+                                                    "name": "Қайсар",
+                                                    "description": "Қожабек Қайсар",
+                                                    "children": []
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            "name": "Биеке",
+                                            "description": "Болатов Биеке",
+                                            "children": [{
+                                                "name": "Руслан",
+                                                "description": "Болатов Руслан",
+                                                "children": [{
+                                                    "name": "Әл-Фараби",
+                                                    "description": "Биеке Әл-Фараби",
+                                                    "children": []
+                                                }]
+                                            }]
+                                        },
+                                        {
+                                            "name": "Мереке",
+                                            "description": "Болатов Мереке",
+                                            "children": [{
+                                                    "name": "Алмер",
+                                                    "description": "Болатов Алмер",
+                                                    "children": []
+                                                },
+                                                {
+                                                    "name": "Қайнар",
+                                                    "description": "Болатов Қайнар",
+                                                    "children": []
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            "name": "Райымбек",
+                                            "description": "Болатов Райымбек",
+                                            "children": [{
+                                                "name": "Манас",
+                                                "description": "Болатов Манас",
+                                                "children": []
+                                            }]
+                                        }
+                                    ]
+                                },
+                                {
+                                    "name": "Жұмабек",
+                                    "description": "Бейсенбаев Жұмабек [Ағатай]",
+                                    "children": [{
+                                            "name": "Айдархан",
+                                            "description": "Бейсенбаев Айдархан",
+                                            "children": [{
+                                                    "name": "Әлихан",
+                                                    "description": "",
+                                                    "children": []
+                                                },
+                                                {
+                                                    "name": "Елнұр",
+                                                    "description": "",
+                                                    "children": []
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            "name": "Қайрат",
+                                            "description": "Бейсенбаев Қайрат",
+                                            "children": []
+                                        }
+                                    ]
+                                },
+                                {
+                                    "name": "Жұмке",
+                                    "description": "Болатов Жұмке [Бәтәй]",
+                                    "children": [{
+                                        "name": "Жанкелді",
+                                        "description": "Болатов Жанкелді",
+                                        "children": []
+                                    }]
+                                },
+                                {
+                                    "name": "Кенжебек",
+                                    "description": "",
+                                    "children": []
+                                }
+                            ]
+                        },
+                        {
+                            "name": "Файызрахман",
+                            "description": "",
+                            "children": [{
+                                "name": "Оразбай",
+                                "description": "",
+                                "children": []
+                            }]
+                        }
+                    ]
+                }]
+            }]
+        }]
+    }]
+}
